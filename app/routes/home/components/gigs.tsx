ee8bf488b3289a48f4e4fd6b5bc4703634ae40be
Lasse Narula <lasse.klovstad@gmail.com>
2026-03-10 11:07:23 +0100

    feat: Fjern gigs hvis tiden er forbi

--- app/routes/home/components/gigs.tsx
+++ app/routes/home/components/gigs.tsx
@@ -8,47 +8,53 @@ const gig = {
 	ticketLink: "https://fast-ansatt.hoopla.no/event/372514305",
 };
 
+const gigExpired = Date.now() - 1000 * 60 * 60 * 24 > gig.date.valueOf();
+
 export const Gigs = () => (
 	<article className="px-4">
 		<H2 className="mb-4">Kommende konserter</H2>
-		<div className="rounded p-6 border border-border">
-			<div className="flex justify-between items-start mb-4">
-				<div>
-					<p className="text-primary font-semibold text-lg">
-						{gig.date.toLocaleDateString(undefined, {
-							dateStyle: "medium",
-						})}
-					</p>
-					<p className="text-muted-foreground text-base">
-						{gig.date.toLocaleDateString(undefined, { weekday: "long" })}
-					</p>
+		{gigExpired ? (
+			<p className="text-xl text-muted-foreground">Ingen planlagte konserter</p>
+		) : (
+			<div className="rounded p-6 border border-border">
+				<div className="flex justify-between items-start mb-4">
+					<div>
+						<p className="text-primary font-semibold text-lg">
+							{gig.date.toLocaleDateString(undefined, {
+								dateStyle: "medium",
+							})}
+						</p>
+						<p className="text-muted-foreground text-base">
+							{gig.date.toLocaleDateString(undefined, { weekday: "long" })}
+						</p>
+					</div>
+				</div>
+
+				<H3 className="mb-2">{gig.venue}</H3>
+				<address className="text-muted-foreground mb-4">{gig.address}</address>
+				<p className="text-sm text-muted-foreground mb-4">
+					🕐 {gig.date.toLocaleTimeString(undefined, { timeStyle: "short" })}
+				</p>
+
+				<div className="flex gap-2">
+					<a
+						target="_blank"
+						rel="noopener noreferrer"
+						href={gig.eventLink}
+						className="bg-primary text-primary-foreground hover:opacity-90 rounded-sm px-4 py-2 text-lg"
+					>
+						Gå til eventside
+					</a>
+					<a
+						target="_blank"
+						rel="noopener noreferrer"
+						href={gig.ticketLink}
+						className="bg-primary text-primary-foreground hover:opacity-90 rounded-sm px-4 py-2 text-lg"
+					>
+						Kjøp billett
+					</a>
 				</div>
 			</div>
-
-			<H3 className="mb-2">{gig.venue}</H3>
-			<address className="text-muted-foreground mb-4">{gig.address}</address>
-			<p className="text-sm text-muted-foreground mb-4">
-				🕐 {gig.date.toLocaleTimeString(undefined, { timeStyle: "short" })}
-			</p>
-
-			<div className="flex gap-2">
-				<a
-					target="_blank"
-					rel="noopener noreferrer"
-					href={gig.eventLink}
-					className="bg-primary text-primary-foreground hover:opacity-90 rounded-sm px-4 py-2 text-lg"
-				>
-					Gå til eventside
-				</a>
-				<a
-					target="_blank"
-					rel="noopener noreferrer"
-					href={gig.ticketLink}
-					className="bg-primary text-primary-foreground hover:opacity-90 rounded-sm px-4 py-2 text-lg"
-				>
-					Kjøp billett
-				</a>
-			</div>
-		</div>
+		)}
 	</article>
 );
